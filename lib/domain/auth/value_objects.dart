0df57a60dd528bc6f7d7ce491796840c6c06e8d8
christian <chrisoftech@gmail.com>
2020-06-08 07:13:48 +0000

    Implemented auth feature value objects

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_todo/a_core/failures/failures.dart';
+import 'package:ddd_todo/a_core/validators/value_objects.dart';
+import 'package:ddd_todo/a_core/validators/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  const EmailAddress._(this.value);
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(validateEmailAddress(input));
+  }
+}
+
+class Password extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  const Password._(this.value);
+
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(validatePassword(input));
+  }
+}
