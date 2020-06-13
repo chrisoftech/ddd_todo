2dde79820dc9b5a9b6cd3c1fac84d34852b3f0e8
christian <chrisoftech@gmail.com>
2020-06-13 19:33:22 +0000

    Added color definitions in note color value object.

--- lib/domain/note/value_objects.dart
+++ lib/domain/note/value_objects.dart
@@ -42,6 +42,16 @@ class TodoName extends ValueObject<String> {
 }
 
 class NoteColor extends ValueObject<Color> {
+  static const List<Color> predefinedColors = [
+    Color(0xfffafafa), // canvas
+    Color(0xfffa8072), // salmon
+    Color(0xfffedc56), // mustard
+    Color(0xffd0f0c0), // tea
+    Color(0xfffca3b7), // flamingo
+    Color(0xff997950), // tortilla
+    Color(0xfffffdd0), // cream
+  ];
+
   @override
   final Either<ValueFailure<Color>, Color> value;
 
